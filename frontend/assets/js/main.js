59a6af11db337b694fe8265587977aeb2f86655f
Nicholas LeBlanc <nicholas.leblanc@paysafe.com>
2016-09-09 23:05:00 -0600

    Working on more styling. Adding in right click menu options, wiring it up.

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,3 +1,20 @@
+buttonTypes = [
+  {
+    name: 'Normal',
+    id: null,
+  },
+  {
+    name: 'Toggle',
+    id: 'toggle',
+  },
+  {
+    name: 'Momentary',
+    id: 'momentary',
+  },
+];
+
+allowedCharacters = ['A', 'B', 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'ENTER', 'ENT', 'ESCAPE', 'BSPACE', 'TAB', 'SPACE', '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|', '#', '~', ';', ':', '‘', '"', '^', ',', '<', '.', '>', '/', '?', 'CAPS', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21', 'F22', 'F23', 'F24', 'PRINT', 'SCROLL', 'PAUSE', 'INSERT', 'HOME', 'PGUP', 'DEL', 'END', 'PGDOWN', 'RIGHT', 'LEFT', 'UP', 'DOWN', 'NUM', 'KPSLASH', 'KPASTERISK', 'KPMINUS', 'KPPLUS', 'KPENTER', 'KP0', 'KP1', 'KP2', 'KP3', 'KP4', 'KP5', 'KP6', 'KP7', 'KP8', 'KP9', 'KPDOT', 'KPEQUAL', 'LCTRL', 'RCTRL', 'LSHIFT', 'RSHIFT', 'LALT', 'TRNS', 'MSTP', 'MPLY', 'MPRV', 'MNXT', 'VOLU', 'VOLD', 'PSCR', 'SLCK', 'MINUS', 'EQUAL', 'LBRACKET', 'RBRACKET', 'BSLASH', 'SCOLON', 'NONUS_HASH', 'QUOTE', 'GRV', 'COMMA', 'DOT', 'SLASH', 'DELETE', 'NLCK', 'RALT', 'LGUI', 'RGUI', 'ESC', 'FN0', 'FN1', 'FN2', 'FN3', 'FN4', 'FN5', 'FN6', 'FN7', 'FN8', 'FN9', 'FN10', 'FN11', 'FN12', 'FN13', 'FN14', 'FN15', 'FN16', 'FN17', 'FN18', 'FN19', 'FN20', 'FN21', 'FN22', 'FN23', 'FN24', 'FN25', 'FN26', 'FN27', 'FN28', 'FN29', 'FN30', 'FN31', 'PRINT', 'SCROLL', '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|', '#', '~', ';', ':', '\'', '"', '`', ',', '<', '.', '>', '/', '?', 'DEL', 'NUM'];
+
 /**
  * Calculates the absolute offset of the provided element
  */
@@ -39,10 +56,13 @@ addLayer = function(layout) {
 };
 
 var v = new Vue({
-  el: '#keyboard-form',
+  el: 'body',
   data: {
     layout: layouts[0]['keys'],
-    template: templates[0]['keys']
+    template: templates[0]['keys'],
+    activeKey: null,
+    buttonTypes: buttonTypes,
+    allowedCharacters: allowedCharacters
   },
   methods: {
     /**
@@ -110,6 +130,10 @@ var v = new Vue({
       } else {
         alert('Cannot delete base layer');
       }
+    },
+
+    focusKey: function(key) {
+      this.activeKey = key;
     }
   }
 });
