fdb9038e65975d41901503c0a291cdc14012f3e9
Nicholas LeBlanc <nicholas.leblanc@paysafe.com>
2016-09-20 22:53:34 -0400

    Fixing up some data mapping in JS. Also enabling hiding the context menu when clicking away from it. If user tabs over, it stays open.

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -15,54 +15,20 @@ buttonTypes = [
 
 allowedCharacters = ['A', 'B', 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'ENTER', 'ENT', 'ESCAPE', 'BSPACE', 'TAB', 'SPACE', '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|', '#', '~', ';', ':', '‘', '"', '^', ',', '<', '.', '>', '/', '?', 'CAPS', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21', 'F22', 'F23', 'F24', 'PRINT', 'SCROLL', 'PAUSE', 'INSERT', 'HOME', 'PGUP', 'DEL', 'END', 'PGDOWN', 'RIGHT', 'LEFT', 'UP', 'DOWN', 'NUM', 'KPSLASH', 'KPASTERISK', 'KPMINUS', 'KPPLUS', 'KPENTER', 'KP0', 'KP1', 'KP2', 'KP3', 'KP4', 'KP5', 'KP6', 'KP7', 'KP8', 'KP9', 'KPDOT', 'KPEQUAL', 'LCTRL', 'RCTRL', 'LSHIFT', 'RSHIFT', 'LALT', 'TRNS', 'MSTP', 'MPLY', 'MPRV', 'MNXT', 'VOLU', 'VOLD', 'PSCR', 'SLCK', 'MINUS', 'EQUAL', 'LBRACKET', 'RBRACKET', 'BSLASH', 'SCOLON', 'NONUS_HASH', 'QUOTE', 'GRV', 'COMMA', 'DOT', 'SLASH', 'DELETE', 'NLCK', 'RALT', 'LGUI', 'RGUI', 'ESC', 'FN0', 'FN1', 'FN2', 'FN3', 'FN4', 'FN5', 'FN6', 'FN7', 'FN8', 'FN9', 'FN10', 'FN11', 'FN12', 'FN13', 'FN14', 'FN15', 'FN16', 'FN17', 'FN18', 'FN19', 'FN20', 'FN21', 'FN22', 'FN23', 'FN24', 'FN25', 'FN26', 'FN27', 'FN28', 'FN29', 'FN30', 'FN31', 'PRINT', 'SCROLL', '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|', '#', '~', ';', ':', '\'', '"', '`', ',', '<', '.', '>', '/', '?', 'DEL', 'NUM'];
 
-/**
- * Calculates the absolute offset of the provided element
- */
-cumulativeOffset = function(element) {
-  var top = element.offsetHeight;
-  var left = 0;
-
-  // Continue looping through elements to build total offset
-  do {
-    top += element.offsetTop  || 0;
-    left += element.offsetLeft || 0;
-    element = element.offsetParent;
-  } while(element);
-
-  return {
-    top: top,
-    left: left
-  };
-};
-
-/**
- * Creates a new template layer based on provided layout
- */
-addLayer = function(layout) {
-  var layers = [];
-
-  for (var i = 0; i < layout.length; i++) {
-    layers[i] = [];
-
-    for (var j = 0; j < layout[i].length; j++) {
-      layers[i][j] = {
-        value: 'TRNS',
-        type: null
-      };
-    };
-  };
-
-  return layers;
-};
-
 var v = new Vue({
   el: 'body',
   data: {
-    layout: layouts[0]['keys'],
-    template: templates[0]['keys'],
-    activeKey: null,
-    buttonTypes: buttonTypes,
-    allowedCharacters: allowedCharacters
+    layout: layouts[0]['keys'], // Layout
+    template: templates[0]['keys'], // Template
+    activeKey: null, // Currently active key
+    buttonTypes: buttonTypes, // List of button types
+    allowedCharacters: allowedCharacters, // Allowed set of characters
+    contextMenuVisible: false, // Show we be showing the context menu?
+    contextMenuPosition: { // Position of the context menu
+      top: 0,
+      left: 0
+    },
+    layerLimit: 33 // Maximum number of layers
   },
   methods: {
     /**
@@ -83,26 +49,60 @@ var v = new Vue({
     },
 
     /**
-     * Show right click menu
+     * Set the position of the context menu
      */
-    showMenu: function(event) {
-      event.preventDefault();
-
+    positionContextMenu: function (event) {
       // Get current key and it's position'
       var key = event.srcElement;
-      var position = cumulativeOffset(key);
+      var position = this.cumulativeOffset(key);
 
-      // Set the position of the tooltip and show it
-      document.getElementById('key-list').style.top = position.top + 'px';
-      document.getElementById('key-list').style.left = position.left + 'px';
-      document.getElementById('key-list').style.display = 'block';
+      Vue.nextTick(function () {
+        // Set the position of the tooltip and show it
+        this.contextMenuPosition.top = position.top;
+        this.contextMenuPosition.left = position.left;
+      }.bind(this));
     },
 
     /**
-     * Hide right click menu
+     * Show context menu
      */
-    hideMenu: function(event) {
-      document.getElementById('key-list').style.display = 'none';
+    showContextMenu: function(event) {
+      event.preventDefault();
+
+      this.contextMenuVisible = true;
+    },
+
+    /**
+     * Hide context menu
+     */
+    hideContextMenu: function (event) {
+      var button = event.which || event.button;
+
+      if (button === 1 && !this.clickInsideContextMenu(event)) {
+        this.contextMenuVisible = false;
+      }
+    },
+
+    /**
+     * Check if the click is inside the context menu
+     *
+     * @returns boolean True if inside, false if outside
+     */
+    clickInsideContextMenu: function (event) {
+      var element = event.srcElement || event.target;
+      var className = 'context-menu';
+
+      if ( element.classList.contains(className) ) {
+        return element;
+      } else {
+        while ( element = element.parentNode ) {
+          if ( element.classList && element.classList.contains(className) ) {
+            return element;
+          }
+        }
+      }
+
+      return false;
     },
 
     /**
@@ -111,9 +111,9 @@ var v = new Vue({
     addLayer: function(event) {
       event.preventDefault();
 
-      // @TODO: Limiting users at 15 total layers, can change if we want
-      if (this.template.length < 15) {
-        this.template.push(addLayer(this.layout));
+      // Limiting users at 33 total layers
+      if (this.template.length < this.layerLimit) {
+        this.template.push(this.createNewLayer(this.layout));
       } else {
         alert('Cannot add any more layers.')
       }
@@ -134,8 +134,52 @@ var v = new Vue({
       }
     },
 
-    focusKey: function(key) {
+    /**
+     * Set the active key and reposition the context menu
+     */
+    focusKey: function(key, event) {
       this.activeKey = key;
+      this.positionContextMenu(event);
+    },
+
+    /**
+     * Calculates the absolute offset of the provided element
+     */
+    cumulativeOffset: function(element) {
+      var top = element.offsetHeight;
+      var left = 0;
+
+      // Continue looping through elements to build total offset
+      do {
+        top += element.offsetTop  || 0;
+        left += element.offsetLeft || 0;
+        element = element.offsetParent;
+      } while(element);
+
+      return {
+        top: top,
+        left: left
+      };
+    },
+
+    /**
+     * Creates a new template layer based on provided layout
+     */
+    createNewLayer: function(layout) {
+      var layers = [];
+
+      for (var i = 0; i < layout.length; i++) {
+        layers[i] = [];
+
+        for (var j = 0; j < layout[i].length; j++) {
+          layers[i][j] = {
+            value: 'TRNS',
+            type: null
+          };
+        };
+      };
+
+      return layers;
     }
   }
 });
